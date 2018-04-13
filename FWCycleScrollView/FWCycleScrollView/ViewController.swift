03e6ad75cb184dc7dcb2cd55b2a492c5d5fc9489
xfg <853299701@qq.com>
2018-04-13 19:15:26 +0800

    swift 仿直播间礼物列表/产品分类列表

--- FWCycleScrollView/FWCycleScrollView/ViewController.swift
+++ FWCycleScrollView/FWCycleScrollView/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     let adArray = ["ad_1", "ad_2", "ad_3"]
     let adArray2 = ["ad_4", "ad_5", "ad_6"]
     let adArray3 = ["ad_5", "ad_6", "ad_7"]
-    let adArray4 = ["ad_1", "ad_2", "ad_3", "ad_4", "ad_5", "ad_6", "ad_7"]
+    let adArray4 = ["ad_1", "ad_2"]
     
     let netAdArray = ["http://pic2.16pic.com/00/10/46/16pic_1046407_b.jpg",
                       "http://pic.58pic.com/58pic/14/34/62/39S58PIC9jV_1024.jpg",
@@ -130,7 +130,6 @@ class ViewController: UIViewController {
         cycleScrollView.pageDotColor = UIColor.red
         cycleScrollView.currentPageDotColor = UIColor.red
         cycleScrollView.pageControlDotSize = CGSize(width: 12, height: 12)
-        cycleScrollView.autoScrollTimeInterval = 2.0
         cycleScrollView.itemDidClickedBlock = { (index) in
             print("当前点击了第\(index + 1)个广告位")
         }
@@ -162,8 +161,38 @@ class ViewController: UIViewController {
         return cycleScrollView
     }()
     
+    /// 例八：仿直播间礼物列表/产品分类列表
+    lazy var cycleScrollView8: FWCycleScrollView = {
+        
+        let cycleScrollView = FWCycleScrollView.cycle(frame: CGRect(x: 0, y: self.cycleScrollView7.frame.maxY + 20, width: self.view.frame.width, height: self.view.frame.width/2 + 30), loopTimes: 1)
+        cycleScrollView.viewArray = self.setupCustomSubView(subViewType: 0)
+        cycleScrollView.currentPageDotEnlargeTimes = 1.0
+        cycleScrollView.customDotViewType = .hollow
+        cycleScrollView.pageDotColor = UIColor.red
+        cycleScrollView.currentPageDotColor = UIColor.red
+        cycleScrollView.pageControlDotSize = CGSize(width: 12, height: 12)
+        cycleScrollView.autoScroll = false
+        return cycleScrollView
+    }()
+    
+    /// 例八：仿直播间礼物列表/产品分类列表
+    lazy var cycleScrollView9: FWCycleScrollView = {
+        
+        let cycleScrollView = FWCycleScrollView.cycle(frame: CGRect(x: 0, y: self.cycleScrollView8.frame.maxY + 20, width: self.view.frame.width, height: self.view.frame.width/2 + 30), loopTimes: 1)
+        cycleScrollView.viewArray = self.setupCustomSubView(subViewType: 1)
+        cycleScrollView.currentPageDotEnlargeTimes = 1.0
+        cycleScrollView.customDotViewType = .hollow
+        cycleScrollView.pageDotColor = UIColor.red
+        cycleScrollView.currentPageDotColor = UIColor.red
+        cycleScrollView.pageControlDotSize = CGSize(width: 12, height: 12)
+        cycleScrollView.autoScroll = false
+        return cycleScrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.lightGray
         
         self.view.addSubview(self.scrollView)
         
@@ -174,9 +203,14 @@ class ViewController: UIViewController {
         self.scrollView.addSubview(self.cycleScrollView5)
         self.scrollView.addSubview(self.cycleScrollView6)
         self.scrollView.addSubview(self.cycleScrollView7)
+        self.scrollView.addSubview(self.cycleScrollView8)
+        self.scrollView.addSubview(self.cycleScrollView9)
         
-        self.scrollView.contentSize = CGSize(width: self.view.bounds.width, height: self.cycleScrollView7.frame.maxY + 20)
+        self.scrollView.contentSize = CGSize(width: self.view.bounds.width, height: self.cycleScrollView9.frame.maxY + 20)
     }
+}
+
+extension ViewController {
     
     func setupUIView(index: Int) -> UILabel {
         
@@ -191,10 +225,79 @@ class ViewController: UIViewController {
     func setupUIView2(index: Int) -> UILabel {
         
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 30))
-        label.backgroundColor = UIColor.lightGray
-        label.textColor = UIColor.white
+        label.backgroundColor = UIColor.white
+        label.textColor = UIColor.black
         label.text = "第 \(index + 1) 张自定义视图"
         return label
+    }
+    
+    func setupUIView3(index: Int, frame: CGRect) -> UIView {
+        
+        let view = UIView(frame: frame)
+        let imageView = UIImageView(image: UIImage(named: "gift_\(index%20)"))
+        imageView.center = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
+        view.addSubview(imageView)
+        return view
+    }
+    
+    func setupUIView4(index: Int, frame: CGRect) -> UIView {
+        
+        let view = UIView(frame: frame)
+        
+        let tmpW = view.frame.width * 0.6
+        let tmpH = view.frame.height * 0.6
+        
+        let label = UILabel(frame: CGRect(x: (view.frame.width - tmpW) / 2, y:  (view.frame.height - tmpH) / 2, width: tmpW, height: tmpH))
+        label.frame.size.height = view.frame.height * 0.6
+        label.backgroundColor = UIColor(red: CGFloat(arc4random()%256)/255.0, green: CGFloat(arc4random()%256)/255.0, blue: CGFloat(arc4random()%256)/255.0, alpha: 1.0)
+        label.textColor = UIColor.white
+        label.text = "\(index + 1)"
+        label.font = UIFont.systemFont(ofSize: 25)
+        label.textAlignment = .center
+        label.layer.cornerRadius = label.frame.width / 2
+        label.clipsToBounds = true
+        view.addSubview(label)
+        
+        return view
+    }
+    
+    func setupCustomSubView(subViewType: Int) -> [UIView] {
+        var tmpX: CGFloat = 0
+        var tmpY: CGFloat = 0
+        
+        let horizontalRow = 4
+        let verticalRow = 2
+        
+        let tmpW = self.view.frame.width / CGFloat(horizontalRow)
+        let tmpH = self.view.frame.width / 2 / CGFloat(verticalRow)
+        
+        var bottomView: UIView?
+        
+        var customSubViewArray: [UIView] = []
+        
+        for index in 0...30 {
+            
+            tmpX = CGFloat(index % horizontalRow) * tmpW
+            if (index % (horizontalRow * verticalRow)) < horizontalRow {
+                tmpY = 0
+            } else {
+                tmpY = tmpH
+            }
+            
+            if bottomView == nil || (index % (horizontalRow * verticalRow) == 0) {
+                bottomView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width / 2 + 30))
+                bottomView?.backgroundColor = UIColor.white
+                customSubViewArray.append(bottomView!)
+            }
+            
+            if subViewType == 0 {
+                bottomView?.addSubview(self.setupUIView3(index: index, frame: CGRect(x: tmpX, y: tmpY, width: tmpW, height: tmpH)))
+            } else {
+                bottomView?.addSubview(self.setupUIView4(index: index, frame: CGRect(x: tmpX, y: tmpY, width: tmpW, height: tmpH)))
+            }
+        }
+        
+        return customSubViewArray
     }
 }
 
